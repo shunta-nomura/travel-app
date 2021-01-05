f5a6d79b78f0da2e982e688384063ab44838f8f6
shunta-nomura <58507009+shunta-nomura@users.noreply.github.com>
2021-01-05 12:48:01 +0900

    アクティブハッシュ設定

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -4,6 +4,18 @@ class MemoriesController < ApplicationController
   end
 
   def create
-    
+    @memorie = Memorie.new(memorie_params)
+    if @memorie.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def memorie_params
+    params.require(:memorie).permit(:description, :image, :prefecture_id).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/memorie.rb
+++ app/models/memorie.rb
@@ -1,4 +1,7 @@
 class Memorie < ApplicationRecord
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+
+  validates :prefecture_id, presence: true
 end
