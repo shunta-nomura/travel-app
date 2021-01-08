17541d43a386593cbc50f8d216fbd58afac0c208
shunta-nomura <58507009+shunta-nomura@users.noreply.github.com>
2021-01-08 17:45:53 +0900

    トップページhtml作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,17 +2,18 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-require("@rails/ujs").start()
-//require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require('jquery')
-require("../flame")
-
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
+
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+require("../flame")
+require("top/main")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
