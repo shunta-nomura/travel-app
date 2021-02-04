44a4324eea2483b0a125ca6e065c033cba170534
shunta-nomura <58507009+shunta-nomura@users.noreply.github.com>
2021-02-04 16:28:36 +0900

    マイページ、お気に入り画像登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def show
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    current_user.update(favorite_params)
+    redirect_to user_path
+  end
+  private
+
+  def favorite_params
+    params.require(:user).permit(:image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "prefectures#index"
   resources :prefectures, only:[:index, :show]
   resources :memories, only:[:new, :create, :show, :destroy]
+  resources :users, only:[:show, :edit, :update]
 end
